ac7f7d2039d816c80463daa55d3dd6d702396c01
oka1691 <oka557817@gmail.com>
2025-03-15 15:55:11 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  const childList = document.createElement('h2');
+  text.textContent = 'ボタンをクリックしました';
+  text.appendChild(childList);
+});
